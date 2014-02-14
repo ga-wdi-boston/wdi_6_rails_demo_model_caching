c605de754c0a1b6239e844d7c1c5cd9766a0b424
Tom Dyer <tdyer@ourstage.com>
2014-02-13 22:25:22 -0500

    Removed one SQL query that gets the count of the Article comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ActiveRecord::Base
   has_many :comments
+
+  def cached_comments_count
+    # Create a key from the self.cache_key and the string
+    # 'comments_count'
+    # EX:
+    # ["articles/10-20140214024749529002000", 'comments_count']
+    # "articles/10-20140214024749529002000comments_count"
+    Rails.cache.fetch([self, 'comments_count']) { comments.size }
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :article
+  belongs_to :article, touch: true
   belongs_to :user
 end
